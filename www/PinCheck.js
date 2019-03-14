c928e338acc996d75c097bc8f2c6fb047577ae22
jordan-strong <jstrong3@byu.edu>
2019-03-14 15:43:57 -0600

    Updated PinCheck.js to work with Outsystems

--- www/PinCheck.js
+++ www/PinCheck.js
@@ -1,5 +1,17 @@
 var exec = require('cordova/exec');
 
-exports.isPinSetup = function(success, error) {
+function PinCheck() {}
+
+PinCheck.prototype.isPinSetup = function(success, error) {
     exec(success, error, "PinCheck", "isPinSetup", ["arg0"]);
 };
+
+// Installation constructor that binds ToastyPlugin to window
+PinCheck.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.PinCheck = new PinCheck();
+  return window.plugins.PinCheck;
+};
+cordova.addConstructor(PinCheck.install);
